f35ca7ae8822b3eb905fa9e072b36b445d75c328
Hugo Molaudzi <57953645+In-Cyberspace@users.noreply.github.com>
2020-03-05 09:29:48 +0200

    Correcting previous commit as not all changes were puched. THe changes remain the same: modified the nav bar and the profile detail page styles. Begun construction of the image upload task.

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Data;
 using GammaForums.Models.ApplicationUser;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -39,6 +41,28 @@ namespace GammaForums.Controllers
                     IsAdmin = userRoles.Contains("Admin")
                 }
             );
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadProfileImage(IFormFile file)
+        {
+            if (file is null)
+            {
+                throw new System.ArgumentNullException(nameof(file));
+            }
+
+            string userId = _userManager.GetUserId(User);
+            // Connect to an Azure Storage Account Container.
+            // Get Blob Container.
+
+            // Parse the content disposition header on the http request.
+            // Grab the file name
+
+            // Get a reference to a Block Blob (a particular type of blob that is going to be uploaded to our blob container, which we have an API for so it’s not difficult)
+            // On that block blob, upload our file, using the file name <— file uploaded to cloud
+
+            // Set the user’s profile image to the URI
+            // Redirect to user’s profile page
         }
     }
 }
